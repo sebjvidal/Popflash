8979e06e74ec39e9e4bcac61eadbb1e48f0d6950
sebjvidal <seb.vidal@btinternet.com>
2021-04-11 11:59:18 +0100

    Update VisualEffectView.swift

--- Popflash/Resources/VisualEffectView.swift
+++ Popflash/Resources/VisualEffectView.swift
@@ -8,13 +8,19 @@
 import SwiftUI
 
 struct VisualEffectView: UIViewRepresentable {
+    
     var effect: UIVisualEffect?
     
     func makeUIView(context: UIViewRepresentableContext<Self>) -> UIVisualEffectView {
+        
         UIVisualEffectView()
+        
     }
     
     func updateUIView(_ uiView: UIVisualEffectView, context: UIViewRepresentableContext<Self>) {
+        
         uiView.effect = effect
+        
     }
+    
 }
