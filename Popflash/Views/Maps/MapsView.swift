d5dec5b58700c88df02f2b3d9539e0bdd58d10d7
sebjvidal <seb.vidal@btinternet.com>
2021-04-12 18:43:30 +0100

    Update MapsView.swift code.

--- Popflash/Views/Maps/MapsView.swift
+++ Popflash/Views/Maps/MapsView.swift
@@ -9,12 +9,158 @@ import SwiftUI
 import Kingfisher
 import FirebaseFirestore
 
-var filtered = false
-var filter = "line.horizontal.3.decrease.circle"
-var filterFilled = "line.horizontal.3.decrease.circle.fill"
+struct MapsView: View {
+    
+    @State var statusOpacity = 0.0
+    
+    @StateObject var mapsViewModel = MapsViewModel()
+    
+    @AppStorage("tabSelection") var tabSelection: Int = 0
+    
+    var body: some View {
+        
+        NavigationView {
+            
+            ZStack(alignment: .top) {
+                
+                ScrollView(offsetChanged: {
+                    
+                    let offset = $0.y
+                    
+                    statusOpacity = Double((1 / 42) * -offset)
+                    
+                }) {
+                    
+                    VStack {
+                        
+                        Header()
+                        
+                        MapsList(maps: mapsViewModel.maps)
+                        
+                    }
+                    
+                }
+                .onAppear() {
+                    
+                    if mapsViewModel.maps.isEmpty {
+                        
+                        mapsViewModel.fetchData(ref: Firestore.firestore().collection("maps"))
+                        
+                    }
+                    
+                    tabSelection = 1
+                    
+                }
+                
+                StatusBarBlur(opacity: statusOpacity)
+                
+            }
+//            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarTitle("Maps", displayMode: .inline)
+            .navigationBarHidden(true)
+            
+        }
+        
+    }
+    
+}
+
+private struct StatusBarBlur: View {
+    
+    var opacity: Double
+    
+    var body: some View {
+        
+        VisualEffectView(effect: UIBlurEffect(style: .systemThinMaterial))
+            .frame(height: 47)
+            .edgesIgnoringSafeArea(.top)
+            .opacity(opacity)
+        
+    }
+    
+}
+
+private struct Header: View {
+    
+    var body: some View {
+        
+        VStack {
+
+            Spacer()
+                .frame(height: 48)
+
+            HStack() {
+
+                Text("Maps")
+                    .font(.system(size: 32))
+                    .fontWeight(.bold)
+                    .padding(.leading, 16)
+
+                Spacer()
+
+                MapsFilterMenu()
+
+            }
+
+            Divider()
+                .padding(.top, 2)
+                .padding(.horizontal, 16)
+                .padding(.bottom, 8)
+
+        }
+        
+    }
+    
+}
+
+private struct MapsFilterMenu: View {
+    
+    var menuItems = ["A-Z", "Popularity", "Active Duty", "Reserves"]
+    
+    @AppStorage("maps.listFilter") var listFilter = "Popularity"
+    
+    var body: some View {
+        
+        Menu {
+            
+            ForEach(menuItems, id: \.self) { item in
+                
+                Button {
+                    
+                    listFilter = item
+                    
+                } label: {
+                    
+                    HStack {
+                        
+                        Text(item)
+                        
+                        if listFilter == item {
+                            
+                            Image(systemName: "checkmark")
+                            
+                        }
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        } label: {
+            
+            Image(systemName: "line.horizontal.3.decrease.circle")
+                .font(.system(size: 32))
+                .padding(.trailing, 16)
+            
+        }
+        
+    }
+    
+}
 
 private struct NewButton: View {
-
+    
     var lastAdded: String
     
     var body: some View {
@@ -54,8 +200,6 @@ private struct NewButton: View {
         
         guard let dateThreshold = calendar.date(byAdding: dateComponent, to: currentDate) else { return false }
         
-        print(dateThreshold)
-        
         if lastAddedDate > dateThreshold {
             
             return true
@@ -92,7 +236,7 @@ public struct MapCell: View {
                         .frame(height: UIScreen.screenWidth / 1.777 - 32)
                     
                 }
-
+                
                 ZStack {
                     
                     let processor = CroppingImageProcessor(size: CGSize(width: 1284, height: 1), anchor: CGPoint(x: 0.5, y: 1))
@@ -105,31 +249,31 @@ public struct MapCell: View {
                     VisualEffectView(effect: UIBlurEffect(style: .systemThinMaterial))
                         .frame(height: 80)
                     
-
+                    
                     HStack {
-    
+                        
                         KFImage(URL(string: map.icon))
                             .resizable()
                             .frame(width: 55, height: 55)
                             .padding(.leading, 12)
-    
+                        
                         VStack(alignment: .leading) {
-    
+                            
                             Text(map.name)
                                 .font(.headline)
-    
+                            
                             Text(map.group)
                                 .font(.subheadline)
-    
+                            
                         }
-    
+                        
                         Spacer()
-    
+                        
                         NewButton(lastAdded: map.lastAdded)
-    
+                        
                         Image(systemName: "chevron.right")
                             .padding(.trailing, 12)
-    
+                        
                     }
                     
                 }
@@ -144,249 +288,82 @@ public struct MapCell: View {
     
 }
 
-struct MapsView: View {
+private struct MapsList: View {
     
-    @ObservedObject public var mapsViewModel = MapsViewModel()
-    
-    @State var statusBarBlurOpacity = 1.0
+    var maps: [Map]
     
     @AppStorage("maps.listFilter") var listFilter = "Popularity"
-    @AppStorage("tabSelection") var tabSelection: Int = 0
     
-    // Status bar blur
-    var StatusBarBlur: some View {
-        VisualEffectView(effect: UIBlurEffect(style: .systemThinMaterial))
-            .frame(height: 47)
-            .edgesIgnoringSafeArea(.top)
-    }
-    
-    var Header: some View {
-        VStack {
+    var body: some View {
+        
+        LazyVStack {
             
-            Spacer()
-                .frame(height: 48)
-            
-            HStack() {
-                Text("Maps")
-                    .font(.system(size: 32))
-                    .fontWeight(.bold)
-                    .padding(.leading, 16)
+            ForEach(filteredMaps(mapsList: maps), id: \.self) { map in
                 
-                Spacer()
-                
-                Menu {
+                NavigationLink(destination: MapsDetailView(map: map)){
                     
-                    Button {
-                        
-                        listFilter = "A-Z"
-                        
-                    } label: {
-                        
-                        if listFilter == "A-Z" {
-                            
-                            HStack {
-                                
-                                Text("A-Z")
-                                
-                                Image(systemName: "checkmark")
-                                
-                            }
-                            
-                        } else {
-                            
-                            Text("A-Z")
-                            
-                        }
-                        
-                    }
-                    
-                    Button {
-                        
-                        listFilter = "Popularity"
-                        
-                    } label: {
-                        
-                        if listFilter == "Popularity" {
-                            
-                            HStack {
-                                
-                                Text("Popularity")
-                                
-                                Image(systemName: "checkmark")
-                                
-                            }
-                            
-                        } else {
-                            
-                            Text("Popularity")
-                            
-                        }
-                        
-                    }
-                    
-                    Button {
-                        
-                        listFilter = "Active Duty"
-                        
-                    } label: {
-                        
-                        if listFilter == "Active Duty" {
-                            
-                            HStack {
-                                
-                                Text("Active Duty")
-                                
-                                Image(systemName: "checkmark")
-                                
-                            }
-                            
-                        } else {
-                            
-                            Text("Active Duty")
-                            
-                        }
-                        
-                    }
-                    
-                    Button {
-                        
-                        listFilter = "Reserves"
-                        
-                    } label: {
-                        
-                        if listFilter == "Reserves" {
-                            
-                            HStack {
-                                
-                                Text("Reserves")
-                                
-                                Image(systemName: "checkmark")
-                                
-                            }
-                            
-                        } else {
-                            
-                            Text("Reserves")
-                            
-                        }
-                        
-                    }
-                    
-                } label: {
-                    
-                    Image(systemName: "line.horizontal.3.decrease.circle")
-                        .font(.system(size: 32))
-                        .padding(.trailing, 16)
+                    MapCell(map: map)
+                        .shadow(radius: 6, y: 5)
+                        .padding(.horizontal, 16)
+                        .padding(.bottom, 10)
                     
                 }
+                .buttonStyle(MapCellButtonStyle())
                 
             }
-            
-            Divider()
-                .padding(.top, 2)
-                .padding(.horizontal, 16)
-                .padding(.bottom, 8)
             
         }
         
     }
     
-    @State private var buttonIcon = filter
-    @State private var statusOpacity = 0.0
+    func filteredMaps(mapsList: [Map]) -> [Map] {
+        
+        let filteredMapsList = mapsList.sorted(by: {
+            
+            if listFilter == "Popularity" {
+                return $0.views > $1.views
+            } else if listFilter == "A-Z" {
+                return $0.name < $1.name
+            } else {
+                return $0.name < $1.name
+            }
+            
+        }).filter( {
+            
+            if listFilter == "Active Duty" {
+                return $0.group == "Active Duty"
+            } else if listFilter == "Reserves" {
+                return $0.group == "Reserves"
+            } else {
+                return $0.group != ""
+            }
+            
+        })
+        
+        return filteredMapsList
+        
+    }
+    
+}
+
+private struct LoadingList: View {
     
     var body: some View {
-        NavigationView {
-            ZStack(alignment: .top) {
-                ScrollView(
-                    axes: .vertical,
-                    showsIndicators: true,
-                    offsetChanged: {
-                        
-                        let offset = $0.y
-                        
-                        statusOpacity = Double((1 / 42) * -offset)
-                        
-                    }) {
-                    
-                    Header
-                    
-                    VStack{
-                        
-                        if mapsViewModel.maps.isEmpty {
-                            
-                            ForEach(1..<10, id: \.self) { _ in
-                                
-                                RoundedRectangle(cornerRadius: 15, style: .continuous)
-                                    .foregroundColor(Color("Loading"))
-                                    .frame(height: ((UIScreen.screenWidth - 32) / 1.77) + 80)
-                                    .padding(.horizontal, 16)
-                                    .padding(.bottom, 8)
-                                
-                            }
-                            
-                        } else {
-                            
-                            ForEach(mapsViewModel.maps.sorted(by: {
-                                
-                                if listFilter == "Popularity" {
-                                    return $0.views > $1.views
-                                } else if listFilter == "A-Z" {
-                                    return $0.name < $1.name
-                                } else {
-                                    return $0.name < $1.name
-                                }
-                                
-                            }).filter( {
-                                
-                                if listFilter == "Active Duty" {
-                                    return $0.group == "Active Duty"
-                                } else if listFilter == "Reserves" {
-                                    return $0.group == "Reserves"
-                                } else {
-                                    return $0.group != ""
-                                }
-                                
-                            }), id: \.self) { map in
-                                
-                                NavigationLink(destination: MapsDetailView(map: map)){
-                                    
-                                    MapCell(map: map)
-                                        .shadow(radius: 6, y: 5)
-                                        .padding(.horizontal, 16)
-                                        .padding(.bottom, 10)
-                                    
-                                }
-                                .buttonStyle(MapCellButtonStyle())
-                                
-                            }
-                            
-                        }
-                        
-                    }
-                    
-                    Spacer()
-                        .frame(height: 10)
-                    
-                }
+        
+        VStack {
+            
+            ForEach(1...10, id: \.self) { _ in
                 
-                StatusBarBlur
-                    .opacity(statusOpacity)
+                RoundedRectangle(cornerRadius: 15, style: .continuous)
+                    .foregroundColor(Color("Loading"))
+                    .frame(height: ((UIScreen.screenWidth - 32) / 1.777) + 80)
+                    .padding(.horizontal, 16)
+                    .padding(.bottom, 8)
                 
             }
-            .navigationBarTitleDisplayMode(.inline)
-            .navigationBarTitle("Maps", displayMode: .inline)
-            .navigationBarHidden(true)
-        }
-        .onAppear() {
-            
-            if mapsViewModel.maps.isEmpty {
-                
-                mapsViewModel.fetchData(ref: Firestore.firestore().collection("maps"))
-                
-            }
-
-            tabSelection = 1
             
         }
+                
     }
+    
 }
