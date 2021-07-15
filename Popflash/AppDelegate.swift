8048d99b864a3c844af122b39574e5b89353c4e9
sebjvidal <seb.vidal@btinternet.com>
2021-07-15 20:12:12 +0100

    Update AppDelegate.swift

--- Popflash/AppDelegate.swift
+++ Popflash/AppDelegate.swift
@@ -21,7 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-        
+
         Auth.auth().signInAnonymously() { (authResult, error) in
             
             guard let user = authResult?.user else {
