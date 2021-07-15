0e4b18d6285754ca62d0abccabd10acf47e92c3a
sebjvidal <seb.vidal@btinternet.com>
2021-07-15 20:15:13 +0100

    Remove ContentView_Preview

--- Popflash/Views/ContentView.swift
+++ Popflash/Views/ContentView.swift
@@ -68,17 +68,3 @@ struct ContentView: View {
     }
     
 }
-
-struct ContentView_Previews: PreviewProvider {
-    
-    static var previews: some View {
-        
-        ContentView()
-            .preferredColorScheme(.light)
-        
-        ContentView()
-            .preferredColorScheme(.dark)
-        
-    }
-    
-}
