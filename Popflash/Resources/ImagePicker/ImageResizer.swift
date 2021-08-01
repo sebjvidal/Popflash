113bf5defb007b610ac80c935dfb1135efb79549
sebjvidal <seb.vidal@btinternet.com>
2021-08-01 17:24:26 +0100

    Add ImageResizer UIImage extension

---
+++ Popflash/Resources/ImagePicker/ImageResizer.swift
@@ -0,0 +1,22 @@
+//
+//  ImageResizer.swift
+//  ImageResizer
+//
+//  Created by Seb Vidal on 01/08/2021.
+//
+
+import SwiftUI
+
+extension UIImage {
+    
+    func imageResized(to size: CGSize) -> UIImage {
+        
+        return UIGraphicsImageRenderer(size: size).image { _ in
+            
+            draw(in: CGRect(origin: .zero, size: size))
+            
+        }
+        
+    }
+    
+}
