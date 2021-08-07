2d929be4cd32a3142604fd53aaab73d1cc2967ad
sebjvidal <seb.vidal@btinternet.com>
2021-08-07 17:33:27 +0100

    Add support for NadeFrom helper function in NadesViewModel

---
+++ Popflash/ViewModels/NadesViewModel/NadesViewModel.swift
@@ -0,0 +1,51 @@
+//
+//  NadesViewModel.swift
+//  Popflash
+//
+//  Created by Seb Vidal on 15/02/2021.
+//
+
+import SwiftUI
+import Foundation
+import FirebaseFirestore
+
+class NadesViewModel: ObservableObject {
+    
+    @Published var nades = [Nade]()
+    
+    private var db = Firestore.firestore()
+    private var lastDocument: QueryDocumentSnapshot!
+    
+    func fetchData(ref: Query) {
+        
+        var dbRef = ref
+        
+        if !nades.isEmpty {
+            
+            dbRef = dbRef.start(afterDocument: lastDocument)
+            
+        }
+            
+        dbRef.getDocuments { (querySnapshot, error) in
+            
+            guard let documents = querySnapshot?.documents else {
+
+                return
+                
+            }
+            
+            for document in documents {
+                
+                self.lastDocument = document
+                
+                let nade = nadeFrom(doc: document)
+                
+                if !self.nades.contains(nade) { self.nades.append(nade) }
+                
+            }
+            
+        }
+        
+    }
+    
+}
