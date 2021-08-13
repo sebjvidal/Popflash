49bf7d66e14626703e54f43c617a0b389d77ba74
sebjvidal <seb.vidal@btinternet.com>
2021-08-13 20:19:20 +0100

    Add MapCollectionView

---
+++ Popflash/Views/Favourites/EditFavouriteMapsView/MapCollectionView/MapCollectionView.swift
@@ -0,0 +1,80 @@
+//
+//  MapCollectionView.swift
+//  MapCollectionView
+//
+//  Created by Seb Vidal on 06/08/2021.
+//
+
+import UIKit
+import SwiftUI
+import Kingfisher
+import FirebaseAuth
+import FirebaseFirestore
+
+struct MapCollectionView: UIViewControllerRepresentable {
+    
+    @Binding var maps: [Map]
+    @Binding var selectedMaps: [Map]
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<MapCollectionView>) -> UICollectionViewController {
+        
+        let cellWidth = (UIScreen.main.bounds.width - 64) / 3
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 16
+        layout.minimumInteritemSpacing = 0
+        layout.itemSize = CGSize(width: cellWidth, height: cellWidth * 1.5)
+        
+        let collectionViewController = UICollectionViewController(collectionViewLayout: layout)
+        collectionViewController.collectionView.delegate = context.coordinator
+        collectionViewController.collectionView.dragDelegate = context.coordinator
+        collectionViewController.collectionView.dropDelegate = context.coordinator
+        collectionViewController.collectionView.dataSource = context.coordinator
+        collectionViewController.collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "FavouriteMapCell")
+        collectionViewController.collectionView.alwaysBounceVertical = true
+        collectionViewController.collectionView.dragInteractionEnabled = true
+        collectionViewController.collectionView.contentInset = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+        collectionViewController.collectionView.backgroundColor = .clear
+        
+        loadCollectionViewData(collectionViewController.collectionView)
+        
+        return collectionViewController
+        
+    }
+    
+    func updateUIViewController(_ uiViewController: UICollectionViewController, context: UIViewControllerRepresentableContext<MapCollectionView>) {}
+    
+    func makeCoordinator() -> Coordinator {
+        
+        Coordinator(self)
+        
+    }
+    
+    func loadCollectionViewData(_ collectionView: UICollectionView) {
+        
+        guard let user = Auth.auth().currentUser else { return }
+        
+        if user.isAnonymous { return }
+        
+        let db = Firestore.firestore()
+        let ref = db.collection("users").document(user.uid).collection("maps")
+        
+        ref.getDocuments { snapshot, error in
+            
+            guard let documents = snapshot?.documents else { return }
+            
+            for document in documents {
+                
+                let map = mapFrom(doc: document)
+                
+                self.maps.append(map)
+                
+            }
+            
+            collectionView.reloadData()
+            
+        }
+        
+    }
+    
+}
