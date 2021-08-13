1eeb0276d9f3a160b14014ac0b37979221f15ac9
sebjvidal <seb.vidal@btinternet.com>
2021-08-13 20:08:10 +0100

    Add dateObject helper function

---
+++ Popflash/Resources/Date/DateObjectFrom.swift
@@ -0,0 +1,23 @@
+//
+//  DateObjectFrom.swift
+//  DateObjectFrom
+//
+//  Created by Seb Vidal on 12/08/2021.
+//
+
+import SwiftUI
+
+func dateObject(from dateString: String) -> Date {
+    
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyyMMddHHmmss"
+    
+    guard let date = formatter.date(from: dateString.replacingOccurrences(of: ".0", with: "")) else {
+        
+        return Date()
+        
+    }
+    
+    return date
+    
+}
