92bae07eb9f095fcae1ebe509c4d5d5d2de704a0
sebjvidal <seb.vidal@btinternet.com>
2021-08-24 19:58:08 +0100

    Add support for image in push notifications

---
+++ NotificationServiceExtension/NotificationService.swift
@@ -0,0 +1,34 @@
+//
+//  NotificationService.swift
+//  NotificationServiceExtension
+//
+//  Created by Seb Vidal on 24/08/2021.
+//
+
+import UserNotifications
+import FirebaseMessaging
+
+class NotificationService: UNNotificationServiceExtension {
+
+    var contentHandler: ((UNNotificationContent) -> Void)?
+    var bestAttemptContent: UNMutableNotificationContent?
+
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        self.contentHandler = contentHandler
+        bestAttemptContent = request.content
+            .mutableCopy() as? UNMutableNotificationContent
+        guard let bestAttemptContent = bestAttemptContent else { return }
+        FIRMessagingExtensionHelper().populateNotificationContent(
+            bestAttemptContent,
+            withContentHandler: contentHandler)
+    }
+    
+    override func serviceExtensionTimeWillExpire() {
+        // Called just before the extension will be terminated by the system.
+        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+            contentHandler(bestAttemptContent)
+        }
+    }
+
+}
