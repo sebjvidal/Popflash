a41dee13dcfb6c30fe97e84ccb3646c0fbc2c5e1
sebjvidal <seb.vidal@btinternet.com>
2021-08-24 19:53:02 +0100

    Add subscribeTo, unsubscribeTo helper functions

---
+++ Popflash/Views/Settings/SettingsView/NotificationsView/NotificationsView/SubscribeTo.swift
@@ -0,0 +1,42 @@
+//
+//  SubscribeTo.swift
+//  SubscribeTo
+//
+//  Created by Seb Vidal on 22/08/2021.
+//
+
+import Foundation
+import FirebaseAuth
+import FirebaseFirestore
+import FirebaseMessaging
+
+func subscribe(to topic: String) {
+    
+    guard let user = Auth.auth().currentUser else {
+        
+        return
+        
+    }
+    
+    if user.isAnonymous {
+        
+        return
+        
+    }
+    
+    Messaging.messaging().subscribe(toTopic: topic) { error in
+        
+        if let error = error {
+            
+            print(error)
+            
+        }
+        
+    }
+    
+    let db = Firestore.firestore()
+    let ref = db.collection("users").document(user.uid)
+    
+    ref.updateData(["notifications" : FieldValue.arrayUnion([topic])])
+    
+}
---
+++ Popflash/Views/Settings/SettingsView/NotificationsView/NotificationsView/UnsubscribeFrom.swift
@@ -0,0 +1,42 @@
+//
+//  UnsubscribeFrom.swift
+//  UnsubscribeFrom
+//
+//  Created by Seb Vidal on 22/08/2021.
+//
+
+import Foundation
+import FirebaseAuth
+import FirebaseFirestore
+import FirebaseMessaging
+
+func unsubscribe(from topic: String) {
+    
+    guard let user = Auth.auth().currentUser else {
+        
+        return
+        
+    }
+    
+    if user.isAnonymous {
+        
+        return
+        
+    }
+    
+    Messaging.messaging().unsubscribe(fromTopic: topic) { error in
+        
+        if let error = error {
+            
+            print(error)
+            
+        }
+        
+    }
+    
+    let db = Firestore.firestore()
+    let ref = db.collection("users").document(user.uid)
+    
+    ref.updateData(["notifications" : FieldValue.arrayRemove([topic])])
+    
+}
