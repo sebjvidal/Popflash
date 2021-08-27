06cbb971c0a9b76b4d609315f2a865a0cbca5467
sebjvidal <seb.vidal@btinternet.com>
2021-08-27 15:50:23 +0100

    Add RecentlyViewedViewModelOrder enum

---
+++ Popflash/ViewModels/RecentlyViewedViewModel/RecentlyViewedViewModelOrder.swift
@@ -0,0 +1,16 @@
+//
+//  RecentlyViewedViewModelOrder.swift
+//  RecentlyViewedViewModelOrder
+//
+//  Created by Seb Vidal on 26/08/2021.
+//
+
+import SwiftUI
+
+enum RecentlyViewedViewModelOrder {
+    
+    case newest
+    case oldest
+    case map
+    
+}
