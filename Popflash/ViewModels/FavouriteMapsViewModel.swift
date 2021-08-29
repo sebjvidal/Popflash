f39d39116808c26af78ce9f7c6c4823e57ff50af
sebjvidal <seb.vidal@btinternet.com>
2021-08-29 20:30:09 +0100

    Update FavouriteMapsViewModel, support document references

--- Popflash/ViewModels/FavouriteMapsViewModel.swift
+++ Popflash/ViewModels/FavouriteMapsViewModel.swift
@@ -13,25 +13,71 @@ class FavouriteMapsViewModel: ObservableObject {
     
     @Published var maps = [Map]()
     
+    init() {
+        
+        fetchData()
+        
+    }
+    
     func fetchData() {
         
-        guard let user = Auth.auth().currentUser else { return }
+        guard let user = Auth.auth().currentUser else {
+            
+            return
+            
+        }
         
-        if user.isAnonymous { return }
+        if user.isAnonymous {
+            
+            return
+            
+        }
         
         let db = Firestore.firestore()
-        let ref = db.collection("users").document(user.uid).collection("maps").order(by: "position")
-        
+        let ref = db.collection("users").document(user.uid).collection("maps")
+
         ref.addSnapshotListener { snapshot, error in
-            
-            guard let documents = snapshot?.documents else { return }
-            
-            self.maps = documents.map { querySnapshot -> Map in
-                
-                return mapFrom(doc: querySnapshot)
-                
+
+            guard let documents = snapshot?.documents else {
+
+                return
+
             }
-            
+
+            self.maps.removeAll()
+
+            for document in documents {
+
+                let data = document.data()
+
+                guard let mapRef = data["map"] as? DocumentReference else {
+
+                    return
+
+                }
+
+                let position = data["position"] as? Int ?? 0
+
+                mapRef.getDocument { snapshot, error in
+
+                    guard let document = snapshot else {
+
+                        return
+
+                    }
+
+                    if var map = mapFrom(doc: document) {
+
+                        map.position = position
+
+                        self.maps.append(map)
+
+                    }
+
+                }
+
+            }
+
         }
         
     }
