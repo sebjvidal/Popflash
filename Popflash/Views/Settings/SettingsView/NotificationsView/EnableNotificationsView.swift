3cfada0fba4a3a23971386849f879be71a81774f
sebjvidal <seb.vidal@btinternet.com>
2021-09-02 16:39:13 +0100

    Update EnableNotificationsView Add additional instructions, animations, images

--- Popflash/Views/Settings/SettingsView/NotificationsView/EnableNotificationsView.swift
+++ Popflash/Views/Settings/SettingsView/NotificationsView/EnableNotificationsView.swift
@@ -9,13 +9,62 @@ import SwiftUI
 
 struct EnableNotificationsView: View {
     
+    @State private var selected = 0
+    @State private var isOn = false
+    @State var lockScreen = false
+    @State var notificationCentre = false
+    @State var banners = false
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
 
-        SettingsIconRow()
-        
-        NotificationsRow()
-        
-        EnableNotificationsRow()
+        Group {
+            
+            SettingsIconRow()
+                .scaleEffect(selected == 1 ? 1.05 : 1)
+            
+            PopflashRow()
+                .scaleEffect(selected == 2 ? 1.05 : 1)
+            
+            NotificationsRow()
+                .scaleEffect(selected == 3 ? 1.05 : 1)
+            
+            EnableNotificationsRow(isOn: $isOn)
+                .scaleEffect(selected == 4 ? 1.05 : 1)
+            
+            NotificationsSettingsView(lockScreen: $lockScreen,
+                                      notificationCentre: $notificationCentre,
+                                      banners: $banners)
+                .scaleEffect([5, 6, 7].contains(selected) ? 1.05 : 1)
+            
+        }
+        .animation(.easeInOut(duration: 0.25), value: selected)
+        .onReceive(timer) { input in
+            
+            selected += 1
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.15) {
+                
+                withAnimation {
+                    
+                    isOn = selected >= 4
+                    
+                }
+                
+                lockScreen = selected > 4
+                notificationCentre = selected > 5
+                banners = selected > 6
+                
+            }
+            
+            if selected > 7 {
+                
+                timer.upstream.connect().cancel()
+                
+            }
+
+        }
         
     }
     
@@ -55,7 +104,8 @@ private struct SettingsIconRow: View {
             }
             
         }
-        .padding()
+        .padding([.top, .horizontal])
+        .padding(.bottom, 8)
         .buttonStyle(RoundedTableCell())
         
     }
@@ -73,6 +123,58 @@ private struct SettingsIconRow: View {
             UIApplication.shared.open(settingsURL)
             
         }
+        
+    }
+    
+}
+
+private struct PopflashRow: View {
+    
+    var body: some View {
+        
+        ZStack {
+            
+            RoundedRectangle(cornerRadius: 15, style: .continuous)
+                .foregroundColor(Color("Background"))
+                .cellShadow()
+            
+            VStack(spacing: 0) {
+                
+                Divider()
+                
+                HStack {
+
+                    Image("Icon")
+                        .resizable()
+                        .frame(width: 29, height: 29)
+                        .clipShape(RoundedRectangle(cornerRadius: 6.5, style: .continuous))
+                        .overlay {
+                            
+                            RoundedRectangle(cornerRadius: 6.5, style: .continuous)
+                                .stroke(.tertiary, lineWidth: 0.2)
+                            
+                        }
+                        .padding(.vertical, 10)
+                    
+                    Text("Popflash")
+
+                    Spacer()
+                    
+                    Image(systemName: "chevron.right")
+                        .foregroundStyle(.tertiary)
+                        .font(.callout)
+                        .padding(.trailing)
+                    
+                }
+                
+                Divider()
+                
+            }
+            .padding([.top, .leading, .bottom])
+            
+        }
+        .padding(.vertical, 8)
+        .padding(.horizontal)
         
     }
     
@@ -133,7 +235,8 @@ private struct NotificationsRow: View {
             .padding([.top, .leading, .bottom])
             
         }
-        .padding([.horizontal, .bottom])
+        .padding(.vertical, 8)
+        .padding(.horizontal)
         
     }
     
@@ -141,9 +244,7 @@ private struct NotificationsRow: View {
 
 private struct EnableNotificationsRow: View {
     
-    @State var isOn = false
-    
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @Binding var isOn: Bool
     
     var body: some View {
         
@@ -163,18 +264,11 @@ private struct EnableNotificationsRow: View {
 
                     Spacer()
                     
-                    Toggle("Enable Notifications", isOn: $isOn)
+                    Toggle("Allow Notifications", isOn: $isOn)
                         .labelsHidden()
                         .padding(.vertical, 8)
-                        .onReceive(timer) { input in
-                            
-                            withAnimation {
-                                
-                                isOn.toggle()
-                                
-                            }
-                            
-                        }
+                        .padding(.trailing)
+                        .overlay(.black.opacity(0.001))
                     
                 }
                 
@@ -184,7 +278,116 @@ private struct EnableNotificationsRow: View {
             .padding([.top, .leading, .bottom])
             
         }
-        .padding([.horizontal, .bottom])
+        .padding(.vertical, 8)
+        .padding(.horizontal)
+        
+    }
+    
+}
+
+private struct NotificationsSettingsView: View {
+    
+    @Binding var lockScreen: Bool
+    @Binding var notificationCentre: Bool
+    @Binding var banners: Bool
+    
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    var body: some View {
+        
+        ZStack {
+            
+            RoundedRectangle(cornerRadius: 15, style: .continuous)
+                .foregroundColor(Color("Background"))
+                .cellShadow()
+            
+            LazyVGrid(columns: columns) {
+                
+                VStack {
+                    
+                    Image("Lock_Screen")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(maxWidth: 40)
+                    
+                    Text("Lock Screen")
+                    
+                    Check(checked: $lockScreen)
+                        .padding(.top, 4)
+                    
+                }
+                
+                VStack {
+                    
+                    Image("Notification_Centre")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(maxWidth: 40)
+                    
+                    Text("Notification Centre")
+                        .fixedSize()
+                    
+                    Check(checked: $notificationCentre)
+                        .padding(.top, 4)
+                    
+                }
+                
+                VStack {
+                    
+                    Image("Banners")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(maxWidth: 40)
+                    
+                    Text("Banners")
+                    
+                    Check(checked: $banners)
+                        .padding(.top, 4)
+                    
+                }
+                
+            }
+            .padding(.vertical, 8)
+            .padding()
+            
+        }
+        .font(.system(.footnote))
+        .padding(.vertical, 8)
+        .padding(.horizontal)
+        
+    }
+    
+}
+
+private struct Check: View {
+    
+    @Binding var checked: Bool
+    
+    var body: some View {
+        
+        ZStack {
+            
+            Image("Uncheck")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 21)
+            
+            if checked {
+                
+                Image("Check")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 21)
+                    .transition(.scale)
+                
+            }
+            
+        }
+        .animation(.easeInOut(duration: 0.1), value: checked)
         
     }
     
