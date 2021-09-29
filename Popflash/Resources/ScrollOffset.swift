7496b1b07a57b67c4e8ec5fff7e61a1f6f762d3f
sebjvidal <seb.vidal@btinternet.com>
2021-09-29 20:01:14 +0100

    Add ScrollOffset.swift PreferenceKey

---
+++ Popflash/Resources/ScrollOffset.swift
@@ -0,0 +1,16 @@
+//
+//  ScrollOffsetPreferenceKey.swift
+//  ScrollOffsetPreferenceKey
+//
+//  Created by Seb Vidal on 28/09/2021.
+//
+
+import SwiftUI
+
+struct ScrollOffset: PreferenceKey {
+    
+    static var defaultValue: CGFloat = 0.0
+
+     static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {}
+    
+ }
