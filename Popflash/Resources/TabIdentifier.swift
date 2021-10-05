dab524231d9fcc1817ea5b77d4c0f2a3e7104fcb
sebjvidal <seb.vidal@btinternet.com>
2021-10-05 17:58:22 +0100

    Add TabIdentifier enum

---
+++ Popflash/Resources/TabIdentifier.swift
@@ -0,0 +1,17 @@
+//
+//  TabSelection.swift
+//  TabSelection
+//
+//  Created by Seb Vidal on 04/10/2021.
+//
+
+import SwiftUI
+
+enum TabIdentifier: Hashable {
+    
+    case featured
+    case maps
+    case favourites
+    case settings
+    
+}
