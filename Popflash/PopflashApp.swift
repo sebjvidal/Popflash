e736dd27ebe41b2d49e728d99eac0fed18cd155b
sebjvidal <seb.vidal@btinternet.com>
2021-10-05 17:52:56 +0100

    Add PopflashApp.swifft, use SwiftUI app lifecycle

---
+++ Popflash/PopflashApp.swift
@@ -0,0 +1,25 @@
+//
+//  PopflashApp.swift
+//  PopflashApp
+//
+//  Created by Seb Vidal on 04/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct PopflashApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    var body: some Scene {
+        
+        WindowGroup {
+            
+            ContentView()
+            
+        }
+        
+    }
+    
+}
