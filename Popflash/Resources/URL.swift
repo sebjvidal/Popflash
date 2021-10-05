7fff079a0fa454f9dc6163220b48655ebe2bcf6d
sebjvidal <seb.vidal@btinternet.com>
2021-10-05 17:58:01 +0100

    Add isDeepLink, tabIdentifier, nadeID URL extensions

---
+++ Popflash/Resources/URL.swift
@@ -0,0 +1,65 @@
+//
+//  URL.swift
+//  URL
+//
+//  Created by Seb Vidal on 04/10/2021.
+//
+
+import SwiftUI
+
+extension URL {
+    
+    var isDeepLink: Bool {
+        
+        return scheme == "popflash"
+        
+    }
+    
+    var tabIdentifier: Int? {
+        
+        guard isDeepLink else {
+            
+            return nil
+            
+        }
+        
+        switch host {
+            
+        case "featured":
+            return 0
+            
+        case "maps":
+            return 1
+            
+        case "favourites":
+            return 2
+            
+        case "settings":
+            return 3
+            
+        default:
+            return nil
+            
+        }
+        
+    }
+    
+    var nadeID: String? {
+        
+        guard isDeepLink else {
+            
+            return nil
+            
+        }
+        
+        guard let id = absoluteString.components(separatedBy: "nade?=").last else {
+            
+            return nil
+            
+        }
+        
+        return id
+        
+    }
+    
+}
