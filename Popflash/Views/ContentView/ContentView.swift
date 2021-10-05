44cb203b870f31d6243b6fcba4f4adcba1fbbd26
sebjvidal <seb.vidal@btinternet.com>
2021-10-05 17:58:50 +0100

    Update ContentView, add support for deep linking

--- Popflash/Views/ContentView/ContentView.swift
+++ Popflash/Views/ContentView/ContentView.swift
@@ -7,14 +7,17 @@
 
 import SwiftUI
 import FirebaseAuth
+import FirebaseFirestore
 
 public var standard = UserDefaults.standard
 
 struct ContentView: View {
     
     @State var showWelcomeView = false
-    @State var tabSelection = standard.integer(forKey: "tabSelection")
+    @State var map: Map?
+    @State var nade: Nade?
     
+    @AppStorage("tabSelection") var tabSelection = 0
     @AppStorage("firstLaunch") var firstLaunch = true
     
     var body: some View {
@@ -53,16 +56,26 @@ struct ContentView: View {
                     
                     Image(systemName: "person.fill")
                     Text("Profile")
+                    
                 }
-                
                 .tag(3)
             
         }
         .onAppear(perform: onAppear)
+        .onOpenURL { url in
+            
+            handleURL(url)
+            
+        }
         .sheet(isPresented: $showWelcomeView) {
             
             WelcomeView()
                 .interactiveDismissDisabled()
+            
+        }
+        .sheet(item: $nade) { nade in
+            
+            NadeView(nade: nade)
             
         }
         
@@ -82,6 +95,57 @@ struct ContentView: View {
                 
                 showWelcomeView = true
                 firstLaunch = false
+                
+            }
+            
+        }
+        
+    }
+    
+    func handleURL(_ url: URL) {
+        
+        guard url.isDeepLink else {
+            
+            return
+            
+        }
+        
+        print(url)
+        
+        if let tabIdentifier = url.tabIdentifier {
+            
+            tabSelection = tabIdentifier
+            
+        }
+        
+        if let nadeID = url.nadeID {
+            
+            self.nade = nil
+            
+            fetchNade(withID: nadeID)
+            
+        }
+        
+    }
+    
+    func fetchNade(withID id: String) {
+        
+        let db = Firestore.firestore()
+        let ref = db.collection("nades").whereField("id", isEqualTo: id).limit(to: 1)
+        
+        ref.getDocuments { snapshot, error in
+            
+            guard let documents = snapshot?.documents else {
+                
+                return
+                
+            }
+            
+            for document in documents {
+                
+                let nade = nadeFrom(doc: document)
+                
+                self.nade = nade
                 
             }
             
