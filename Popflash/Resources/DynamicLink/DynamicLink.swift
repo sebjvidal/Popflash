b5f340283570333927060051cf3a730165760e34
sebjvidal <seb.vidal@btinternet.com>
2021-10-17 17:10:26 +0100

    Add DynamicLink Identifiable struct

---
+++ Popflash/Resources/DynamicLink/DynamicLink.swift
@@ -0,0 +1,15 @@
+//
+//  DynamicLink.swift
+//  DynamicLink
+//
+//  Created by Seb Vidal on 17/10/2021.
+//
+
+import SwiftUI
+
+struct DynamicLink: Identifiable {
+    
+    let id = UUID()
+    let link: URL
+    
+}
