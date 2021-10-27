21264c92c8e12c52f35d7e0e731de084a3c3bded
sebjvidal <seb.vidal@btinternet.com>
2021-10-27 21:56:46 +0100

    Add OverviewFrom.swift, overviewFrom(document doc: DocumentSnapshot) function

---
+++ Popflash/ViewModels/OverviewViewModel/OverviewFrom.swift
@@ -0,0 +1,28 @@
+//
+//  OverviewFrom.swift
+//  OverviewFrom
+//
+//  Created by Seb Vidal on 27/10/2021.
+//
+
+import SwiftUI
+import FirebaseFirestore
+
+func overviewFrom(doc document: DocumentSnapshot) -> Overview? {
+    
+    guard let data = document.data() else {
+        
+        return nil
+        
+    }
+    
+    let id = document.documentID
+    let upperLevel = data["upperRadar"] as? String ?? ""
+    let lowerLevel = data["lowerRadar"] as? String
+    let scale = data["scale"] as? Double ?? 1
+    
+    let overview = Overview(id: id, upperRadar: upperLevel, lowerRadar: lowerLevel, scale: scale)
+    
+    return overview
+    
+}
