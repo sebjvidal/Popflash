2eb68dd56de241c13384459df93c47e5eee23af4
sebjvidal <seb.vidal@btinternet.com>
2021-10-27 21:55:42 +0100

    Add Overview struct

---
+++ Popflash/Models/Overview.swift
@@ -0,0 +1,17 @@
+//
+//  Overview.swift
+//  Overview
+//
+//  Created by Seb Vidal on 27/10/2021.
+//
+
+import SwiftUI
+
+struct Overview: Hashable {
+    
+    var id: String
+    var upperRadar: String
+    var lowerRadar: String?
+    var scale: Double
+    
+}
