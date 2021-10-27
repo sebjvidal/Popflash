713b4d01674163a582720770003cdb4ab3adc6c1
sebjvidal <seb.vidal@btinternet.com>
2021-10-27 21:55:22 +0100

    Add Callout model

---
+++ Popflash/Models/Callout.swift
@@ -0,0 +1,16 @@
+//
+//  Callout.swift
+//  Callout
+//
+//  Created by Seb Vidal on 27/10/2021.
+//
+
+import SwiftUI
+
+struct Callout: Hashable {
+    
+    var name: String
+    var posX: Double
+    var posY: Double
+    
+}
