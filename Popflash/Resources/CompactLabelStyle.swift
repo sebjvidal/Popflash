b6e7878c13e802a38347f836c5fef5b41992b6b9
sebjvidal <seb.vidal@btinternet.com>
2021-11-07 21:07:08 +0000

    Add CompactLabelStyle, LabelStyle extension CompactLabelStyle reduces the horizontal spacing between the Label icon and text

---
+++ Popflash/Resources/CompactLabelStyle.swift
@@ -0,0 +1,33 @@
+//
+//  CompactLabelStyle.swift
+//  CompactLabelStyle
+//
+//  Created by Seb Vidal on 07/11/2021.
+//
+
+import SwiftUI
+
+struct CompactLabelStyle: LabelStyle {
+    
+    func makeBody(configuration: Configuration) -> some View {
+        
+        HStack(alignment: .center, spacing: 10) {
+            
+            configuration.icon
+            configuration.title
+            
+        }
+        
+    }
+    
+}
+
+extension LabelStyle where Self == CompactLabelStyle {
+    
+    internal static var compact: CompactLabelStyle {
+        
+        return CompactLabelStyle()
+        
+    }
+    
+}
