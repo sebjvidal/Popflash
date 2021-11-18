01a30faf5e68088385e7b61cf40e50d053c436d8
Seb Vidal <seb.vidal@btinternet.com>
2021-11-18 20:44:11 +0000

    Update URL.swift URL extension. Add check for url components length

--- Popflash/Resources/URL.swift
+++ Popflash/Resources/URL.swift
@@ -10,62 +10,54 @@ import SwiftUI
 extension URL {
     
     var isDeepLink: Bool {
-        
         return scheme == "popflash"
-        
     }
     
     var isDynamicLink: Bool {
-        
         return scheme == "https" && host == "popflash.app"
-        
     }
     
     var tabIdentifier: Int? {
-        
         guard isDeepLink else {
-            
             return nil
-            
         }
         
         switch host {
-            
         case "featured":
             return 0
-            
         case "maps":
             return 1
-            
         case "favourites":
             return 2
-            
         case "settings":
             return 3
-            
         default:
             return nil
-            
         }
-        
     }
     
     var nadeID: String? {
-        
-        guard isDeepLink else {
-            
+        guard isDeepLink, absoluteString.contains("nade?id=") else {
             return nil
-            
         }
         
-        guard let id = absoluteString.components(separatedBy: "nade?=").last else {
-            
+        guard let id = absoluteString.components(separatedBy: "nade?id=").last else {
             return nil
-            
         }
         
         return id
+    }
+    
+    var mapID: String? {
+        guard isDeepLink, absoluteString.contains("map?id=") else {
+            return nil
+        }
         
+        guard let id = absoluteString.components(separatedBy: "map?id=").last else {
+            return nil
+        }
+        
+        return id
     }
     
 }
