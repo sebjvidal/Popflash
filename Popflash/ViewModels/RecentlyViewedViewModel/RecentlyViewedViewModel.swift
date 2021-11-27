3228c8d5b0214f13bf2c08a849e010d17344efd1
sebjvidal <seb.vidal@btinternet.com>
2021-11-27 20:29:20 +0000

    Clean up RecentlyViewedViewModel

--- Popflash/ViewModels/RecentlyViewedViewModel/RecentlyViewedViewModel.swift
+++ Popflash/ViewModels/RecentlyViewedViewModel/RecentlyViewedViewModel.swift
@@ -10,89 +10,62 @@ import FirebaseAuth
 import FirebaseFirestore
 
 class RecentlyViewedViewModel: ObservableObject {
-    
-    @Published var nades = [Nade]()
-    
+    @Published var nades: [Nade] = []
     private var lastDocument: QueryDocumentSnapshot!
     
     func fetchData(order: RecentlyViewedViewModelOrder = .newest) {
-        
         guard let user = Auth.auth().currentUser else {
-            
             return
-            
         }
         
         if user.isAnonymous {
-            
             return
-            
         }
         
         let db = Firestore.firestore()
         let descending = (order == .newest ? true : false)
-        var ref = db.collection("users").document(user.uid).collection("recents").order(by: "dateAdded", descending: descending).limit(to: 25)
+        var ref = db.collection("users").document(user.uid).collection("recents").order(by: "dateAdded", descending: descending).limit(to: 10)
         
         if lastDocument != nil {
-            
             ref = ref.start(afterDocument: lastDocument)
-            
         }
         
         ref.getDocuments { snapshot, error in
-            
             guard let documents = snapshot?.documents else {
-                
                 return
-                
             }
             
             for document in documents {
-                
                 let data = document.data()
-                
                 let dateAdded = data["dateAdded"] as? Double ?? 0
+                
                 guard let recentRef = data["ref"] as? DocumentReference else {
-                    
                     return
-                    
                 }
                 
                 self.lastDocument = document
                 
                 recentRef.getDocument { nadeDocument, error in
-                    
                     guard let nadeDocument = nadeDocument else {
-                        
                         return
-                        
                     }
                     
                     guard var recentNade = nadeFrom(doc: nadeDocument) else {
-                        
                         return
-                        
                     }
                     
                     recentNade.dateAdded = dateAdded
                     
                     self.nades.append(recentNade)
-                    
                 }
-
             }
-            
         }
-        
     }
     
     func refresh() {
-        
         self.nades.removeAll()
         self.lastDocument = nil
         
         fetchData()
-        
     }
-    
 }
