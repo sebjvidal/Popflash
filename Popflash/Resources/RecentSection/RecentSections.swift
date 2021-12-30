e9d7ec4ec3e84a0b795f6ed68b9430b037600678
sebjvidal <seb.vidal@btinternet.com>
2021-12-30 17:43:26 +0000

    Add RecentSections enum for RecentlyViewed section iteration

---
+++ Popflash/Resources/RecentSection/RecentSections.swift
@@ -0,0 +1,19 @@
+//
+//  RecentSection.swift
+//  Popflash
+//
+//  Created by Seb Vidal on 30/12/2021.
+//
+
+import SwiftUI
+
+enum RecentSections {
+    case today
+    case yesterday
+    case last7Days
+    case lastMonth
+    case last3Months
+    case last6Months
+    case lastYear
+    case allTime
+}
