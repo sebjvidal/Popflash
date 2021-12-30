fac22998c99c8e2958ba9d9acc09ba16f07da531
sebjvidal <seb.vidal@btinternet.com>
2021-12-30 17:42:22 +0000

    Add RecentSection identifiable struct for RecentlyViewed section iteration

---
+++ Popflash/Resources/RecentSection/RecentSection.swift
@@ -0,0 +1,14 @@
+//
+//  RecentSection.swift
+//  Popflash
+//
+//  Created by Seb Vidal on 30/12/2021.
+//
+
+import SwiftUI
+
+struct RecentSection: Identifiable {
+    var id = UUID()
+    var title: String
+    var section: RecentSections
+}
