45b08866f6011734e28179b873b5ea2691eb7032
sebjvidal <seb.vidal@btinternet.com>
2021-12-30 17:47:43 +0000

    Update RecentlyViewedViewModel, change document limit to 50, add section(dateAdded: Double) -> String function to support RecentlyViewedView refactor

--- Popflash/ViewModels/RecentlyViewedViewModel/RecentlyViewedViewModel.swift
+++ Popflash/ViewModels/RecentlyViewedViewModel/RecentlyViewedViewModel.swift
@@ -11,6 +11,7 @@ import FirebaseFirestore
 
 class RecentlyViewedViewModel: ObservableObject {
     @Published var nades: [Nade] = []
+    
     private var lastDocument: QueryDocumentSnapshot!
     
     func fetchData(order: RecentlyViewedViewModelOrder = .newest) {
@@ -24,7 +25,7 @@ class RecentlyViewedViewModel: ObservableObject {
         
         let db = Firestore.firestore()
         let descending = (order == .newest ? true : false)
-        var ref = db.collection("users").document(user.uid).collection("recents").order(by: "dateAdded", descending: descending).limit(to: 10)
+        var ref = db.collection("users").document(user.uid).collection("recents").order(by: "dateAdded", descending: descending).limit(to: 50)
         
         if lastDocument != nil {
             ref = ref.start(afterDocument: lastDocument)
@@ -55,11 +56,37 @@ class RecentlyViewedViewModel: ObservableObject {
                     }
                     
                     recentNade.dateAdded = dateAdded
+                    recentNade.section = self.section(dateAdded: dateAdded)
                     
-                    self.nades.append(recentNade)
+                    if !self.nades.contains(recentNade) {
+                        self.nades.append(recentNade)
+                    }
                 }
             }
         }
+    }
+    
+    private func section(dateAdded: Double) -> String {
+        let dateBounds: [(title: String, bound: Date)] = [
+            (title: "Today", bound: date()),
+            (title: "Yesterday", bound: date(subtracting: 1)),
+            (title: "Last Week", bound: date(subtracting: 7)),
+            (title: "Last Month", bound: date(subtracting: 30)),
+            (title: "Last 3 Months", bound: date(subtracting: 91)),
+            (title: "Last 6 Months", bound: date(subtracting: 182)),
+            (title: "Last Year", bound: date(subtracting: 365))
+        ]
+        
+        let dateStr = String(dateAdded)
+        let dateObj = dateObject(from: dateStr)
+        
+        for (title, bound) in dateBounds.reversed() {
+            if dateObj < bound {
+                return title
+            }
+        }
+        
+        return "All Time"
     }
     
     func refresh() {
