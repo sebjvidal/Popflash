7f42ab32d0ae22d385707cc0adf296514fd06d26
sebjvidal <seb.vidal@btinternet.com>
2021-12-30 17:45:09 +0000

    Update date(bound: Bound, subtracting subtraction: Int = 0) -> Date function Use renamed DateBound enum

--- Popflash/Resources/Date/Date.swift
+++ Popflash/Resources/Date/Date.swift
@@ -7,34 +7,28 @@
 
 import SwiftUI
 
-func date(bound: Bound, subtracting subrtraction: Int = 0) -> Date {
-    
+func date(bound: DateBound = .upper, subtracting subrtraction: Int = 0) -> Date {
     let currentDate = Date()
-    
+
     var dateComponents = Calendar.current.dateComponents(
         [.year, .month, .day, .hour, .minute, .second],
         from: currentDate
     )
-    
+
     dateComponents.hour = (bound == .upper ? 24 : 1)
     dateComponents.minute = (bound == .upper ? 59 : 0)
     dateComponents.second = (bound == .upper ? 59 : 0)
-    
+
     var subtractionComponent = DateComponents()
     subtractionComponent.day = -subrtraction
-    
+
     guard let date = Calendar.current.date(from: dateComponents) else {
-        
         return Date()
-        
     }
-        
+
     guard let dateBound = Calendar.current.date(byAdding: subtractionComponent, to: date) else {
-        
         return Date()
-        
     }
-    
+
     return dateBound
-    
 }
