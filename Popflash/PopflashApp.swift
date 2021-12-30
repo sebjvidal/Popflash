ec4e9fd8e190fec23d6ab240d38db50c7cfde28a
sebjvidal <seb.vidal@btinternet.com>
2021-12-30 17:53:07 +0000

    Update PopflashApp.swift, cleanup code

--- Popflash/PopflashApp.swift
+++ Popflash/PopflashApp.swift
@@ -9,17 +9,11 @@ import SwiftUI
 
 @main
 struct PopflashApp: App {
-    
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
     var body: some Scene {
-        
         WindowGroup {
-            
             ContentView()
-            
         }
-        
     }
-    
 }
