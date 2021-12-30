2a753a89849b5ca5febf9bd1823f7dade86f618b
sebjvidal <seb.vidal@btinternet.com>
2021-12-30 17:54:22 +0000

    Update NadesViewModel.swift, cleanup code

--- Popflash/ViewModels/NadesViewModel/NadesViewModel.swift
+++ Popflash/ViewModels/NadesViewModel/NadesViewModel.swift
@@ -10,50 +10,34 @@ import Foundation
 import FirebaseFirestore
 
 class NadesViewModel: ObservableObject {
-    
+
     @Published var nades = [Nade]()
-    
     private var db = Firestore.firestore()
     private var lastDocument: QueryDocumentSnapshot!
     
-    func fetchData(ref: Query) {
-        
+    func fetchData(ref: Query, tagFilter: [String] = []) {
         var dbRef = ref
         
         if !nades.isEmpty {
-            
             dbRef = dbRef.start(afterDocument: lastDocument)
-            
         }
             
         dbRef.getDocuments { (querySnapshot, error) in
-            
             guard let documents = querySnapshot?.documents else {
-
                 return
-                
             }
             
             for document in documents {
-                
                 self.lastDocument = document
                 
                 guard let nade = nadeFrom(doc: document) else {
-                    
                     continue
-                    
                 }
                 
                 if !self.nades.contains(nade) {
-                    
                     self.nades.append(nade)
-                    
                 }
-                
             }
-            
         }
-        
     }
-    
 }
