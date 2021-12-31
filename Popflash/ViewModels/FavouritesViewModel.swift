dbdea49a80ebd909d24cd8c77e9346eaccc9a3e3
sebjvidal <seb.vidal@btinternet.com>
2021-12-31 16:11:53 +0000

    Cleanup FavouritesViewModel.swift, fix favourite nades not being added to Recently Viewed

--- Popflash/ViewModels/FavouritesViewModel.swift
+++ Popflash/ViewModels/FavouritesViewModel.swift
@@ -11,60 +11,42 @@ import FirebaseAuth
 import Foundation
 
 class FavouritesViewModel: ObservableObject {
-    
-    @Published var nades = [Nade]()
+    @Published var nades: [Nade] = []
     
     func fetchData() {
-        
         guard let user = Auth.auth().currentUser else {
-            
             return
-        
         }
         
         if user.isAnonymous {
-            
             return
-            
         }
         
         if nades.isEmpty {
-            
             let db = Firestore.firestore()
             let ref = db.collection("users").document(user.uid).collection("nades").order(by: "dateAdded", descending: true)
             
             ref.addSnapshotListener { querySnapshot, error in
-                
                 guard let documents = querySnapshot?.documents else {
-                    
                     return
-                    
                 }
                 
                 self.nades.removeAll()
                 
                 for document in documents {
-                    
-                    guard let nade = nadeFrom(doc: document) else {
-                        
+                    guard var nade = nadeFrom(doc: document) else {
                         continue
-                        
                     }
                     
+                    nade.documentID = document.data()["documentID"] as? String ?? ""
+                    
                     self.nades.append(nade)
-                                    
                 }
-                
             }
-            
         }
-
     }
     
     func clear() {
-        
         nades.removeAll()
-        
     }
-    
 }
