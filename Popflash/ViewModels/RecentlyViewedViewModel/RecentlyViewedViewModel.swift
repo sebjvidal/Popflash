48397f0fa9c1c8702e3c1afb53f906fb490543e5
sebjvidal <seb.vidal@btinternet.com>
2021-12-31 16:26:12 +0000

    Update RecentlyViewedViewModel, fix "Last 7 Days" bug, add "All Time" date bound

--- Popflash/ViewModels/RecentlyViewedViewModel/RecentlyViewedViewModel.swift
+++ Popflash/ViewModels/RecentlyViewedViewModel/RecentlyViewedViewModel.swift
@@ -70,11 +70,12 @@ class RecentlyViewedViewModel: ObservableObject {
         let dateBounds: [(title: String, bound: Date)] = [
             (title: "Today", bound: date()),
             (title: "Yesterday", bound: date(subtracting: 1)),
-            (title: "Last Week", bound: date(subtracting: 7)),
-            (title: "Last Month", bound: date(subtracting: 30)),
-            (title: "Last 3 Months", bound: date(subtracting: 91)),
-            (title: "Last 6 Months", bound: date(subtracting: 182)),
-            (title: "Last Year", bound: date(subtracting: 365))
+            (title: "Last 7 Days", bound: date(subtracting: 2)),
+            (title: "Last Month", bound: date(subtracting: 8)),
+            (title: "Last 3 Months", bound: date(subtracting: 31)),
+            (title: "Last 6 Months", bound: date(subtracting: 91)),
+            (title: "Last Year", bound: date(subtracting: 182)),
+            (title: "All Time", bound: date(subtracting: 365))
         ]
         
         let dateStr = String(dateAdded)
