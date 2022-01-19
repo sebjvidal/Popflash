75ef325e6ed8b336138a4d95956cf04348f52a25
sebjvidal <seb.vidal@btinternet.com>
2022-01-19 20:32:58 +0000

    Update ImageResizer.swift, cleanup code

--- Popflash/Resources/ImagePicker/ImageResizer.swift
+++ Popflash/Resources/ImagePicker/ImageResizer.swift
@@ -8,15 +8,9 @@
 import SwiftUI
 
 extension UIImage {
-    
     func imageResized(to size: CGSize) -> UIImage {
-        
         return UIGraphicsImageRenderer(size: size).image { _ in
-            
             draw(in: CGRect(origin: .zero, size: size))
-            
         }
-        
     }
-    
 }
