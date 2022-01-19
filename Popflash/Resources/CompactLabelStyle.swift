7737120b4eb9770da1315eee6c38d40640a5891b
sebjvidal <seb.vidal@btinternet.com>
2022-01-19 20:35:52 +0000

    Update CompactLabelStyle.swift

--- Popflash/Resources/CompactLabelStyle.swift
+++ Popflash/Resources/CompactLabelStyle.swift
@@ -8,26 +8,16 @@
 import SwiftUI
 
 struct CompactLabelStyle: LabelStyle {
-    
     func makeBody(configuration: Configuration) -> some View {
-        
         HStack(alignment: .center, spacing: 10) {
-            
             configuration.icon
             configuration.title
-            
         }
-        
     }
-    
 }
 
 extension LabelStyle where Self == CompactLabelStyle {
-    
     internal static var compact: CompactLabelStyle {
-        
         return CompactLabelStyle()
-        
     }
-    
 }
