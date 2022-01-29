891d9ea40b4a72b90c411d0d5ed8e1a227a09210
sebjvidal <seb.vidal@btinternet.com>
2022-01-29 18:43:54 +0000

    Update AppIcon struct, add premium property

--- Popflash/Views/Settings/AppIcon/AppIcon.swift
+++ Popflash/Views/Settings/AppIcon/AppIcon.swift
@@ -11,10 +11,12 @@ struct AppIcon: Identifiable, Hashable {
     var id = UUID()
     var name: String
     var asset: String?
+    var premium: Bool
     
-    init(name: String, asset: String?) {
+    init(name: String, asset: String?, premium: Bool = false) {
         self.name = name
         self.asset = asset
+        self.premium = premium
     }
 }
 
