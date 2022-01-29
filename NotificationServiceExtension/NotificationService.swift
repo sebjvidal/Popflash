e97a16e5d6c7e0403ce84501f62365f8643459cf
sebjvidal <seb.vidal@btinternet.com>
2022-01-29 18:41:32 +0000

    Update NotificationService.swift, cleanup code

--- NotificationServiceExtension/NotificationService.swift
+++ NotificationServiceExtension/NotificationService.swift
@@ -9,36 +9,23 @@ import UserNotifications
 import FirebaseMessaging
 
 class NotificationService: UNNotificationServiceExtension {
-
     var contentHandler: ((UNNotificationContent) -> Void)?
     var bestAttemptContent: UNMutableNotificationContent?
 
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
-        
         self.contentHandler = contentHandler
-        
         bestAttemptContent = request.content.mutableCopy() as? UNMutableNotificationContent
         
         guard let bestAttemptContent = bestAttemptContent else {
-            
             return
-            
         }
         
-        FIRMessagingExtensionHelper().populateNotificationContent(
-            bestAttemptContent,
-            withContentHandler: contentHandler)
-        
+        FIRMessagingExtensionHelper().populateNotificationContent(bestAttemptContent, withContentHandler: contentHandler)
     }
     
     override func serviceExtensionTimeWillExpire() {
-        
         if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
-            
             contentHandler(bestAttemptContent)
-            
         }
-        
     }
-
 }
