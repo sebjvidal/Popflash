8e863589a27870b4a8b10404cc7052bd50c84be5
sebjvidal <seb.vidal@btinternet.com>
2022-01-29 18:42:31 +0000

    Update Map struct, conform to Favouriteable

--- Popflash/Models/Map.swift
+++ Popflash/Models/Map.swift
@@ -6,9 +6,10 @@
 //
 
 import SwiftUI
+import FirebaseAuth
+import FirebaseFirestore
 
-struct Map: Codable, Hashable, Identifiable {
-    
+struct Map: Codable, Hashable, Identifiable, Favouriteable {
     var id: String
     var name: String
     var group: String
@@ -21,4 +22,39 @@ struct Map: Codable, Hashable, Identifiable {
     var favourite: Bool
     var position: Int
     
+    func getFavourite(completion: @escaping (Bool) -> Void) {
+        guard let user = Auth.auth().currentUser, !user.isAnonymous else {
+            completion(false)
+            return
+        }
+        
+        let db = Firestore.firestore()
+        let mapRef = db.collection("maps").document(id)
+        let ref = db.collection("users").document(user.uid).collection("maps").whereField("map", isEqualTo: mapRef)
+        
+        ref.getDocuments { snapshot, error in
+            guard let documents = snapshot?.documents else {
+                completion(false)
+                return
+            }
+            
+            completion(!documents.isEmpty)
+        }
+    }
+}
+
+extension Map {
+    static let preview = Map(
+        id: "dust2",
+        name: "Dust II",
+        group: "Active Duty",
+        scenario: "Bomb Defusal",
+        background: "https://firebasestorage.googleapis.com/v0/b/popflash-3e8b8.appspot.com/o/Backgrounds%2Fdust2_background.jpg?alt=media&token=fe15f376-15e6-4b3a-a6b5-35a5a7f184f7",
+        radar: "",
+        icon: "https://firebasestorage.googleapis.com/v0/b/popflash-3e8b8.appspot.com/o/Icons%2Fdust2_icon.png?alt=media&token=14a8732e-aa82-4d9a-9d8f-437f77f226b0",
+        views: 0,
+        lastAdded: "",
+        favourite: false,
+        position: 0
+    )
 }
